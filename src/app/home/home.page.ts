086c7b41c6171f832d4f3d17b1fbe7763f5cf3dc
Armindo Malafaia Neto <malafaia.armindo@hotmail.com>
2021-10-19 13:33:54 -0400

    Challenge Duplicates

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,33 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  name = ['ball', 'bat', 'glove', 'glove', 'glove'];
+  price = [2,3,1,2,1];
+  weight = [2,5,1,1,1];
+
+  nrDuplicates = 0;
+
   constructor() {}
+
+  checkDuplicates(nameStr, priceStr, weightStr) {
+    let count = 0;
+    const resultData = new Map();
+    let i = 0;
+    for (i=0; i < nameStr.length; i++) {
+       const currentProduct = nameStr[i] + priceStr[i] + weightStr[i];
+       if (resultData.get(currentProduct) != null) {
+          count++;
+       } else {
+          resultData.set(currentProduct, 1);
+       };
+    };
+    console.log(count);
+    return count;
+  }
+
+  onClickCount() {
+    this.nrDuplicates = this.checkDuplicates(this.name, this.price, this.weight);
+  };
+
 
 }
